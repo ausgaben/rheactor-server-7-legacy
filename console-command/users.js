c25db4a3ae46ad207bb45f36d3d28440160e3d9e
Markus Tacker <m@cto.hiv>
2016-06-06 12:07:41 +0200

    chore(CLI): show if a user active

--- console-command/users.js
+++ console-command/users.js
@@ -1,11 +1,19 @@
 'use strict'
 
+const colors = require('colors')
+
 module.exports = {
   description: 'list all users',
   action: (backend) => {
     return backend.repositories.user.findAll()
       .map((user) => {
-        console.log('-', user.aggregateId(), user.name(), user.email.toString())
+        console.log(
+          '-',
+          user.aggregateId(),
+          user.name(),
+          user.email.toString(),
+          user.isActive ? colors.green('[active]') : colors.red('[inactive]')
+        )
       })
   }
 }
