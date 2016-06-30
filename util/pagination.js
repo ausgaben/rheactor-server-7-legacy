d6552fac4a77e1738a13fa55f1de7186a6dbcb4b
Markus Tacker <m@cto.hiv>
2016-06-30 14:38:50 +0200

    feat(Pagination): extract PaginatedResult model

--- util/pagination.js
+++ util/pagination.js
@@ -1,6 +1,7 @@
 'use strict'
 
-let maxItemsPerPage = 100
+const maxItemsPerPage = 100
+const PaginatedResult = require('../model/paginated-result')
 
 /**
  * @param {number} offset
@@ -43,28 +44,6 @@ Pagination.prototype.result = function (items, total, query) {
     nextOffset = self.offset + self.itemsPerPage
   }
   return new PaginatedResult(items, total, self.itemsPerPage, self.offset, query, prevOffset, nextOffset)
-}
-
-/**
- * Represents a paginated result
- *
- * @param {Array} items The actual items on the current page
- * @param {Number} total The total number of items
- * @param {Number} itemsPerPage The number of items per page
- * @param {Number} offset The offset of the current page
- * @param {object} query The query object used for the result
- * @param {Number|undefined} prevOffset If set represents the offset to use in order to fetch the previous page
- * @param {Number|undefined} nextOffset If set represents the offset to use in order to fetch the next page
- * @constructor
- */
-function PaginatedResult (items, total, itemsPerPage, offset, query, prevOffset, nextOffset) {
-  Object.defineProperty(this, 'items', {value: items, enumerable: true})
-  Object.defineProperty(this, 'total', {value: total, enumerable: true})
-  Object.defineProperty(this, 'itemsPerPage', {value: itemsPerPage, enumerable: true})
-  Object.defineProperty(this, 'offset', {value: offset, enumerable: true})
-  Object.defineProperty(this, 'query', {value: query, enumerable: true})
-  Object.defineProperty(this, 'prevOffset', {value: prevOffset, enumerable: true})
-  Object.defineProperty(this, 'nextOffset', {value: nextOffset, enumerable: true})
 }
 
 module.exports = Pagination
